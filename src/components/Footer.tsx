f7b265065839315525c12ecca106362cba0b12ea
luciok1203 <luciokim123@gmail.com>
2026-01-08 23:42:06 +0900

    biome check

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,11 +1,9 @@
 const Footer = () => {
   return (
     <footer>
-      <div>
-        마지막 업데이트: 0일 0시간 전
-      </div>
+      <div>마지막 업데이트: 0일 0시간 전</div>
     </footer>
-  )
-}
+  );
+};
 
 export default Footer;
