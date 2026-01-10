e7167f8996e61c6e3ee0988c917390e5624af14d
Lucio <79063845+luciok1203@users.noreply.github.com>
2026-01-11 03:57:51 +0900

    ✨routes, Header, Footer (#2)
    * routes, header, footer 추가
    
    * biome check
    
    * 불필요 의존성 제거
    
    * PR템플릿 수정

---
+++ src/components/Header.tsx
@@ -0,0 +1,9 @@
+const Header = () => {
+  return (
+    <header>
+      <div>스누보드</div>
+    </header>
+  );
+};
+
+export default Header;
---
+++ src/pages/HomePage.tsx
@@ -0,0 +1,5 @@
+const HomePage = () => {
+  return <div>스누보드에 오신 것을 환영합니다!</div>;
+};
+
+export default HomePage;
